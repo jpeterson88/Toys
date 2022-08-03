0bdb30d2756ccd58b6e057f6f75b06577f2bc504
Justin Peterson <jpeterson4223@yahoo.com>
2022-08-03 15:59:29 -0700

    minor tweaks

--- Assets/Scripts/GroundedDetector.cs
+++ Assets/Scripts/GroundedDetector.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Assets.Scripts.Utility;
+using UnityEngine;
 
 namespace Assets.Scripts
 {
@@ -10,19 +11,28 @@ namespace Assets.Scripts
         [SerializeField]
         private LayerMask detectableLayers;
 
+        [SerializeField] private BoxCaster boxCaster;
+
         private bool isGrounded;
 
         public bool IsGrounded() => isGrounded;
 
-        private void Update()
+        //private void Update()
+        //{
+        //    var hit = Physics2D.Raycast(transform.position, Vector2.down, distance, detectableLayers);
+
+        //    isGrounded = hit ? hit.transform : null;
+
+        //    Color castColor = Physics2D.Raycast(transform.position, Vector2.down, distance, detectableLayers) ? Color.red : Color.green;
+
+        //    Debug.DrawRay(transform.position, new Vector2(0, distance), castColor);
+
+        //    var downDetected = Physics2D.BoxCastAll(gameObject.transform.position, boxDetectSize, 0, Vector2.down, detectDistance, detectableLayers);
+        //}
+
+        void Update()
         {
-            var hit = Physics2D.Raycast(transform.position, Vector2.down, distance, detectableLayers);
-
-            isGrounded = hit ? hit.transform : null;
-
-            Color castColor = Physics2D.Raycast(transform.position, Vector2.down, distance, detectableLayers) ? Color.red : Color.green;
-
-            Debug.DrawRay(transform.position, new Vector2(0, distance), castColor);
+            isGrounded = boxCaster.IsHit();
         }
     }
 }
---
+++ Assets/Scripts/Utility/BoxCaster.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Utility
+{
+    class BoxCaster : MonoBehaviour
+    {
+        [SerializeField] private float distance;
+        [SerializeField] private LayerMask layer;
+
+        private bool isHit;
+        RaycastHit2D hit;
+
+        private void Update()
+        {
+            hit = Physics2D.BoxCast(transform.position, transform.lossyScale / 1.5f, 0f, Vector2.down, distance, layer);
+
+            isHit = hit.transform != null;
+        }
+
+        public bool IsHit() => isHit;
+
+        void OnDrawGizmos()
+        {
+            if (isHit)
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawRay(transform.position, Vector2.down * hit.distance);
+                Gizmos.DrawWireCube(transform.position + Vector3.down * hit.distance, transform.lossyScale);
+            }
+            else
+            {
+                Gizmos.color = Color.green;
+                Gizmos.DrawRay(transform.position, Vector3.down * distance);
+            }
+        }
+    }
+}
